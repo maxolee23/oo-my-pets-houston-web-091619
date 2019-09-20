417d7813b4480e19112a1a471958be251bb49e15
Max Lee <maxolee23@gmail.com>
2019-09-20 00:07:52 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,15 +3,15 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
+  @@all = []
+
   def initialize(name, owner, mood="nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
   end
 
-  # def mood(mood="nervous")
-  #   @mood = mood
-  # end
 
 
 
