2d2dc3fce3b50fdb942f52a1fe22e238e37195db
Max Lee <maxolee23@gmail.com>
2019-09-20 00:11:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,13 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
 
+  @@all = []
+
   def initialize(name, owner, mood="nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
   end
 
   # def mood(mood="nervous")
